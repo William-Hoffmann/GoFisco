1d530553a338881c0f45b92b0e0d769c29a910cb
William-Hoffmann <willhausgaming@gmail.com>
2019-11-30 15:48:57 -0300

    DB Config

--- config/database.php
+++ config/database.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Str;
 
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
+
 return [
 
     /*
@@ -15,7 +21,8 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    /*'default' => env('DB_CONNECTION', 'mysql'),*/
+	'default' => env('DB_CONNECTION', 'your_heroku_mysql_connection'),
 
     /*
     |--------------------------------------------------------------------------
@@ -43,6 +50,17 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
+		'your_heroku_mysql_connection' => array(
+			'driver' => 'mysql',
+			'host' => $host,
+			'database' => $database,
+			'username' => $username,
+			'password' => $password,
+			'charset' => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix' => '',
+		),
+		
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
